04e9bf38a27fee4eb1fdfefa1a98fd657c96e443
Michael Lozitckii <m.lozitskii@infusion.games>
2024-06-21 00:04:44 +0300

    feat: first page init, + indexation

---
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+import styles from './MainPage.module.scss';
+
+export const MainPage = () => {
+  return (
+    <div>
+      temp
+      <div>temp2</div>
+    </div>
+  );
+};
