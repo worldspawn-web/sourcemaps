b7dbe1e1886075c8fb3413e7e4936d07ba09047b
Michael Lozitckii <m.lozitskii@infusion.games>
2024-06-27 22:29:47 +0300

    feat: main page maps sorting

--- frontend/src/common/RenderMaps/RenderMaps.tsx
+++ frontend/src/common/RenderMaps/RenderMaps.tsx
@@ -1,28 +1,37 @@
-import data from '../../data/maps.json';
 import styles from './RenderMaps.module.scss';
+import data from '../../data/maps.json';
 
 export const RenderMaps = () => {
-  const mapsData = data;
+  const mapsData = data.maps;
+  const noImageLink = '../public/no_image_available.png';
+
+  const renderCards = (map) => {
+    const { name, game, gamemode, date } = map;
+    let { link, image, tournament } = map;
+
+    if (!image) image = noImageLink;
+    if (!link) link = '#';
+    if (!tournament) tournament = false;
+
+    return (
+      <div className={styles.maps__map}>
+        <a href={link}>
+          <img src={image} className={styles.maps__image} />
+          <span className={styles.maps__name}>{name}</span>
+        </a>
+      </div>
+    );
+  };
+
+  const sortedMaps = mapsData.sort((a, b) => {
+    const dateA = new Date(a.date);
+    const dateB = new Date(b.date);
+    return dateB.getTime() - dateA.getTime();
+  });
 
   return (
     <section className={styles.maps__wrapper}>
-      {mapsData.maps.map((map) => {
-        const { name, game, gamemode, date } = map;
-        let { link, image, tournament } = map;
-
-        if (!image) image = '../public/avatar.jpg';
-        if (!link) link = '#';
-        if (!tournament) tournament = false;
-
-        return (
-          <div className={styles.maps__map}>
-            <a href={link}>
-              <img src={image} className={styles.maps__image} />
-              <span className={styles.maps__name}>{name}</span>
-            </a>
-          </div>
-        );
-      })}
+      {sortedMaps.map((map) => renderCards(map))}
     </section>
   );
 };
