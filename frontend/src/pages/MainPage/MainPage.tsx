dd3b6247f4db7222d6f032f601d836fa0aa3b87c
Michael Lozitckii <m.lozitskii@infusion.games>
2024-06-27 13:38:33 +0300

    feat: MainPage redo with RenderMaps component

--- frontend/src/pages/MainPage/MainPage.tsx
+++ frontend/src/pages/MainPage/MainPage.tsx
@@ -1,38 +1,15 @@
-import { Background, Container, ContainerHeader } from '../../widgets';
+import { Background, Container } from '../../widgets';
 import { Navigation } from '../../common';
-import { levelCalculate } from '../../helpers';
 
 import styles from './MainPage.module.scss';
-
-const MainPageBio = () => {
-  return (
-    <section className={styles.mp__wrapper}>
-      <img className={styles.mp__avatar} src="../public/avatar.jpg" />
-      {/* redo with separate component */}
-      <section>
-        <table>
-          <tr>
-            <td className={styles.mp__info_header}>Status:</td>
-            <td className={styles.mp__info}>Online 24/7</td>
-          </tr>
-          <tr>
-            <td className={styles.mp__info_header}>Level:</td>
-            <td className={styles.mp__info}>{levelCalculate()}</td>
-          </tr>
-        </table>
-      </section>
-    </section>
-  );
-};
+import { RenderMaps } from '../../common/RenderMaps/RenderMaps';
 
 export const MainPage = () => {
   return (
     <Background>
       <Navigation />
       <Container>
-        {/* {consider removing} */}
-        <ContainerHeader>Worldspawn Mapping Portfolio</ContainerHeader>
-        <MainPageBio />
+        <RenderMaps />
       </Container>
     </Background>
   );
