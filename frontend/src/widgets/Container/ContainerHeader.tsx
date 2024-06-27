9a375a267dd92328b3734052f04440638ac2a8d1
Michael Lozitckii <m.lozitskii@infusion.games>
2024-06-27 13:36:40 +0300

    fix: removed container header component, fixed general container margin

--- frontend/src/widgets/Container/ContainerHeader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Container.module.scss';
-
-interface ContainerHeader {
-  children: React.ReactNode;
-}
-
-export const ContainerHeader: React.FC<ContainerHeader> = ({ children }) => {
-  return <h1 className={styles.container__header}>{children}</h1>;
-};
