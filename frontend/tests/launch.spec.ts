6c01e80a057909fb25aecb6bbd98c43efc5650c3
Michael Lozitckii <m.lozitskii@infusion.games>
2024-06-28 15:59:06 +0300

    feat(tests): nav tests

--- frontend/tests/launch.spec.ts
+++ frontend/tests/launch.spec.ts
@@ -1,8 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('localhost:5173/');
+const navigateToLink = async (page, linkText, expectedUrl) => {
+  await page.getByText(linkText).click();
+  await expect(page).toHaveURL(expectedUrl);
+  await page.goto('localhost:5173');
+};
 
-  // Expect a title "to contain" a substring.
+test.beforeEach(async ({ page }) => {
+  await page.goto('localhost:5173');
+});
+
+test('basic tests', async ({ page }) => {
   await expect(page).toHaveTitle(/SourceMaps/);
+});
+
+test('navigation links', async ({ page }) => {
+  await navigateToLink(page, 'Maps', 'https://gamebanana.com/members/1695181');
+  await navigateToLink(
+    page,
+    'YouTube',
+    'https://www.youtube.com/channel/UCGFGwp-vh5FVAKxrqL1Xicg'
+  );
+  await navigateToLink(page, 'GitHub', 'https://github.com/worldspawn-web');
 });
